c7829917a9242fb7d7b27118ade686d96bc22af8
PVS1905 <viktor66462@gmail.com>
2026-04-10 22:54:05 +0300

    Solution

--- city/models.py
+++ city/models.py
@@ -14,6 +14,7 @@ class City(Base):
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     name: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
     additional_info: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
+    temperatures = relationship('Temperature', back_populates='city', cascade='all, delete-orphan')
 
 
 class Temperature(Base):
--- city/routes.py
+++ city/routes.py
@@ -1,56 +1,37 @@
-from typing import Annotated
 from fastapi import APIRouter
-from datetime import datetime
 from sqlalchemy import select
 from fastapi import Depends
 from sqlalchemy.orm import Session
 from city import schemas, crud, models
 from city.crud import fetch_temperature
 from database.deps import get_db
+from datetime import datetime, timezone
 
 city_router = APIRouter()
 
-@city_router.get("/cities/", response_model=list[schemas.CitySchemaDetail])
-def get_cities(
-    db: Annotated[Session, Depends(get_db)],
-    limit: int | None = None,
-):
+@city_router.get("/cities", response_model=list[schemas.CitySchemaDetail])
+def get_cities(db: Session = Depends(get_db), limit: int | None = None):
     return crud.get_city(db=db, limit=limit)
 
 
-@city_router.post("/city/", response_model=schemas.CitySchemaDetail)
-def create_city(
-    city: schemas.CitySchemaBase,
-    db: Session = Depends(get_db),
-
-):
+@city_router.post("/cities", response_model=schemas.CitySchemaDetail)
+def create_city(city: schemas.CitySchemaBase, db: Session = Depends(get_db)):
     return crud.create_city(db=db, city=city)
 
 
-@city_router.get("/city/{city_id}/", response_model=schemas.CitySchemaDetail)
-def get_city(
-    db: Annotated[Session, Depends(get_db)],
-    city_id: int,
-):
+@city_router.get("/cities/{city_id}", response_model=schemas.CitySchemaDetail)
+def get_city(city_id: int, db: Session = Depends(get_db)):
     return crud.get_city_by_id(db=db, city_id=city_id)
 
 
-@city_router.put("/city/{city_id}/", response_model=schemas.CitySchemaDetail)
-def get_city(
-    db: Annotated[Session, Depends(get_db)],
-    city_id: int,
-    city: schemas.CitySchemaBase
-):
+@city_router.put("/cities/{city_id}", response_model=schemas.CitySchemaDetail)
+def update_city(city_id: int, city: schemas.CitySchemaBase, db: Session = Depends(get_db)):
     return crud.city_update(db=db, city_id=city_id, city=city)
 
 
-@city_router.delete("/city/{city_id}/", status_code=204)
-def get_city(
-    db: Annotated[Session, Depends(get_db)],
-    city_id: int,
-):
+@city_router.delete("/cities/{city_id}", status_code=204)
+def delete_city(city_id: int, db: Session = Depends(get_db)):
     return crud.city_delete(db=db, city_id=city_id)
-
 
 @city_router.post("/temperatures/update")
 async def temperatures_update(db: Session = Depends(get_db)):
@@ -61,30 +42,24 @@ async def temperatures_update(db: Session = Depends(get_db)):
 
         db_temp = models.Temperature(
             city_id=city.id,
-            date_time=datetime.utcnow(),
+            date_time=datetime.now(timezone.utc),
             temperature=temp
         )
 
         db.add(db_temp)
 
     db.commit()
-
     return {"message": "Temperatures updated"}
 
 
 @city_router.get("/temperatures", response_model=list[schemas.TemperatureSchema])
-def get_temperatures(db: Session = Depends(get_db)):
-    return db.scalars(select(models.Temperature)).all()
-
-
-@city_router.get("/temperatures/", response_model=list[schemas.TemperatureSchema])
-def get_temperatures_by_city(
+def get_temperatures(
     city_id: int | None = None,
     db: Session = Depends(get_db)
 ):
     query = select(models.Temperature)
 
-    if city_id:
+    if city_id is not None:
         query = query.where(models.Temperature.city_id == city_id)
 
     return db.scalars(query).all()
